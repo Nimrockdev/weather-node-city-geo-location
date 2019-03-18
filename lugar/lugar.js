04de511dc9da5ee00393b326e8c34a0a6a4a03e5
Jsanchez <jsanchez@fedefarma.com>
2019-03-18 12:38:25 +0100

    Primer commit apliacion clima 2, nuevos servicios de ubicación

---
+++ lugar/lugar.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+
+const getLugarLatLng = async(dir) => {
+    const encodedUlr = encodeURI(dir);
+
+    const instance = axios.create({
+        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ encodedUlr }`,
+        headers: { 'X-RapidAPI-Key': 'YrIv9XHJxmmshCBitpg1YTAnahQSp1KbdHhjsnSBU1hvMDMlzK' }
+    });
+
+    const resp = await instance.get();
+
+    if (resp.data.Results.length === 0) {
+        throw new Error(`No hay resultados para ${ dir }`);
+    }
+
+    const data = resp.data.Results[0];
+    const direccion = data.name;
+    const latitud = data.lat;
+    const longitud = data.lon;
+
+
+    return {
+        direccion,
+        latitud,
+        longitud
+    }
+
+    /*let encodeURL = encodeURI(direccion);
+    let resp = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURL}&key=AIzaSyDyJPPlnIMOLp20Ef1LlTong8rYdTnaTXM`)
+
+    if (resp.data.status === 'ZERO_RESULTS') {
+        throw new Error(`No se han encontrado resultados para ${direccion}`)
+    }
+
+    let location = resp.data.results[0];
+    let coordenadas = location.geometry.location;
+
+    return {
+        direccion: location.formatted_address,
+        latitud: coordenadas.lat,
+        longitud: coordenadas.lng
+    }
+*/
+}
+
+module.exports = {
+    getLugarLatLng
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+#Leer
+npm init
+npm i --save yargs
+
+Instalar con ```npm install```
+npm install axios --save
+
+#Ejemplos
+```
+node app -d Madrid España
+```
+
+# Librerias
+request (callbacks) o axios (utiliza promesas)
+
+#git 
+git add .
+git commit -m "Commit inicial"
+git remote add origin https://github.com/jsg8405/weather-node.git
+git  push -u origin master
+git tag -a v1.0.0 -m "Tag 1"
+git push --tags
